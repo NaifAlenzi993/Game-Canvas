e0b0f406ecfe3f049747a1ea668c99bfa1abd4fb
NaifAlenzi993 <Naif.Alryadh.rae.js@tuwaiq.edu.sa>
2022-01-15 22:57:55 +0300

    g

--- script.js
+++ script.js
@@ -1,134 +1,244 @@
+window.addEventListener("load", function (e) {
+    const mouseParagraph = document.getElementById("mouse");
+    const rectpos = document.getElementById("rectpos");
+  // const score = document.getElementById("score")
+    const bullitinto = document.getElementById("bulltenemy");
+    const correctshots =  document.getElementById("correctshots")
+    const stateGame = document.getElementById("state")
+    const theCanve = document.getElementById("myCa");
+    const ctx = theCanve.getContext("2d");
+
+    const btnStartGame = document.getElementById("btnStart")
+    const btnStopGame = document.getElementById("btnStop")
+
+
+    let bulletEnemy_x = 100 + Math.floor(Math.random() * 300);
+    let bulletEnemy_y = 10;
+    let bulletEnemy_speed = 10;
+    let bulletEnemy_size = 20
+
+    let bulletEnemyArr = []
+
+    const rect_w = 100 , rect_h = 20;
+
+    let rect_x = 500 / 2 - rect_w / 2;
+    let rect_y = 444;
+
+    let myBullet_x = rect_x  + rect_w/2-10
+    let myBullet_y = rect_y - 10
+
+    let myBullets = [] 
+    let myBulletsTime = 100
+    let myBulletsSpeed = 10
+    let myBulletsSize = 10
+    let spaceMove = 30;
+
+    let ShotEvent = false
+
+    let startGame = 1
+
+    let countCorrectShots = 0
+
+
+  this.setInterval(() => {
+    if (startGame == 1) {
+
+        rectpos.innerHTML = `RectPosition : X = ${rect_x} , Y = ${rect_y}`;
+        bullitinto.innerHTML = `BulletEnemy : X = ${bulletEnemy_x} , Y = ${bulletEnemy_y}`;
+    
+        ctx.clearRect(0, 0, 500, 500);
+
+        //check 
+        myBullets.forEach((bullet,i) => {
+            if (
+                (bullet.x > bulletEnemy_x - 20 ) && (bullet.x < bulletEnemy_x + 20) 
+                && (bullet.y <= bulletEnemy_y + 26.5)){
+                    countCorrectShots++
+                    correctshots.innerHTML = `correctShots :  ${countCorrectShots} 
+                     `
+                    bulletEnemy_y = 10
+                    bulletEnemy_x = 100 + Math.floor(Math.random() * 350);
+                    myBullets.splice(i,1)
+            }
+        })
 
     
-    window.addEventListener("load" , function (e) {
-        const mouseParagraph = document.getElementById("mouse")
-        const keyboard = document.getElementById("keyboard")
-        const score = document.getElementById("score")
+        myBullet()
+    
+        enemyBullet()
+    
+        rectControl()
+    
+    }else if (startGame == -1){ 
+        ctx.clearRect(0, 0, 500, 500);
+        ctx.beginPath();
+        ctx.font = "60px Arial";
+        ctx.fillStyle = "red";
+        ctx.fillText("You Are Loser !", 50, 500/2);
+        ctx.closePath();
+    }else if (stateGame == 0) {
+        ctx.clearRect(0, 0, 500, 500);
+        ctx.beginPath();
+        ctx.font = "60px Arial";
+        ctx.fillStyle = "red";
+        ctx.fillText("Stop Game !", 50, 500/2);
+        ctx.closePath();
+    }
+   
+    
+ }, 100);
 
-        const theCanve = document.getElementById("myCa")
-        const ctx = theCanve.getContext("2d")
 
+    window.addEventListener("keydown", (e) => {
 
-        let bullet_x = Math.floor(Math.random() * 400);
-        let bullet_y = 10
-        let bullet_speed = 15
-
-
-        let rect_x = 500/2 - 180/2
-        let rect_y = 444
-        const rect_w = 180 , rect_h = 30
-        let spaceMove = 25
-
-        this.setInterval(()=>{
-            if (bullet_y > 420){
-                bullet_x = Math.floor(Math.random() * 400);
-                ctx.clearRect(0,0,500,440);
-                bullet_y = 10
-            }
-
-            bullet_y += bullet_speed
-
-            ctx.beginPath();
-            ctx.rect(bullet_x, bullet_y, 10, 10);
-            ctx.fillStyle= "red"
-            ctx.fill();
-            ctx.closePath()
-
-        } , 200)
-
+    if (e.keyCode == "37") { // left
+      rect_x -= spaceMove;
+    } else if (e.keyCode == "39") { // right
+      rect_x += spaceMove;
+    } else if (e.keyCode == "32") { //Space
+        if (startGame !== -1 || stateGame !== 0) {
+            myBullet_x = rect_x  + rect_w/2-myBulletsSize/2
+            myBullet_y = rect_y - 10
+            // this.setTimeout
+            // ShotEvent = true
+            myBullets.push({x:  myBullet_x , y: myBullet_y , speed: myBulletsSpeed , size : myBulletsSize})
+        }
        
         
+        // if (myBullets.length == 1){
+        //     this.setInterval(newBullet, myBulletsTime)
+        // }
+    }
+
+
+  });
+
+  function rectControl() {
+
+    // ctx.beginPath();
+    // ctx.rect(rect_x, rect_y, rect_w, rect_h);
+    // ctx.fillStyle = "orange";
+    // ctx.fill();
+    // ctx.closePath();
+
+
+    if (rect_x > 390) {
+        rect_x = 390;
+    }
+
+    if (rect_x < 16) {
+        rect_x = 16;
+    }
+
+    ctx.beginPath();
+    ctx.rect(rect_x, rect_y, rect_w, rect_h);
+    ctx.fillStyle = "orange";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  function myBullet() {
+        if (myBullets.length){
+            myBullets.forEach(bullet => {
+                bullet.y -= bullet.speed
+                ctx.beginPath();
+                // ctx.rect(bullet.x, bullet.y, bullet.size, bullet.size);
+                // ctx.beginPath();
+                ctx.arc(bullet.x, bullet.y, bullet.size, 0, 2 * Math.PI);
+                ctx.fillStyle = "orange";
+                ctx.stroke()
+                ctx.strokeStyle = "red"
+                ctx.fill();
+                ctx.closePath();
+            });
+        }
+  }
+
+  function enemyBullet() {
+    if (bulletEnemy_y > 415) {
+        
+        setTimeout(()=>{   
+            stateGame.innerHTML = "Started Game"
+            bulletEnemy_x = 150 + Math.floor(Math.random() * 300);
+            // ctx.clearRect(0, 0, 500, 444);
+            bulletEnemy_y = 10;
+            startGame = 1
+        } , 2500)
+
+        myBullets = []
+        startGame = -1
+        stateGame.innerHTML = "You are Loser :("
+    }
+        
+    
+        bulletEnemy_y += bulletEnemy_speed;
+    
         ctx.beginPath();
-        ctx.rect(rect_x, rect_y, rect_w, rect_h);
-        ctx.fillStyle= "orange"
+        ctx.rect(bulletEnemy_x, bulletEnemy_y, 20, 20);
+        ctx.fillStyle = "red";
         ctx.fill();
-        ctx.closePath()
+        ctx.closePath();
+  }
 
-        window.addEventListener("keydown" , (e) => {
-            ctx.clearRect(0,444,500,500);
-            
-            if (e.keyCode == '37') {
-                keyboard.innerHTML = "left"
-                rect_x -= spaceMove
-            }
+    btnStartGame.addEventListener("click" , ()=> {
+        startGame = 1
+    })
 
-            else if (e.keyCode == '39') {
-                keyboard.innerHTML = "right"
-                rect_x += spaceMove
-            }
+    btnStopGame.addEventListener("click" , ()=> {
+        startGame = 0
+    })
 
-            // if (rect_y < 444 || rect_y > 444) {
-            //     rect_y = 444 
-            // }
+  window.addEventListener("mousemove", (e) => {
+    mouseParagraph.innerHTML = `MousePosition : X = ${e.clientX}  , Y = ${e.clientY}`;
+    const w = rect_w;
+    const h = rect_h;
 
-            if (rect_x > 305){
-                rect_x = 305
-            }
+    // let x = e.clientX;
+    // let y = e.clientY;
 
-            if (rect_x < 16) {
-                rect_x = 16
-            }
+    
 
-            ctx.beginPath();
-            ctx.rect(rect_x, rect_y, rect_w, rect_h);
-            ctx.fillStyle= "orange"
-            ctx.fill();
-            ctx.closePath()
-        })
+    // if (y < 415 || y > 415) {
+    //     y = 415
+    // }
 
+    // if (x > 404){
+    //     x = 404
+    // }
 
-        window.addEventListener("mousemove" , (e)=> {
-            mouseParagraph.innerHTML = `X = ${e.clientX}  , Y = ${e.clientY}`
-            const w = 180
-            const h = 30
+    // if (x < 94) {
+    //     x = 94
+    // }
 
-            let x = e.clientX
-            let y = e.clientY
+    // let posX = x - w/2
+    // let posY = y - h/2
 
-            // if (y < 471 || y > 471) {
-            //     y = 471 
-            // }
+    // const ww = rect_w
+    // const hh = rect_h
 
-            // if (x > 404){
-            //     x = 404
-            // }
+    // ctx.clearRect(0,0,500,500);
 
-            // if (x < 94) {
-            //     x = 94
-            // }
+    // // clear()
+    // ctx.beginPath()
+    // ctx.strokeRect(x - ww/2, y - hh/2, ww, hh)
 
-            // let posX = x - w/2
-            // let posY = y - h/2
+    // ctx.closePath()
 
-            // const ww = 190
-            // const hh = 40
-
-            // ctx.clearRect(0,444,500,500);
-
-            // // clear()
-            // ctx.beginPath()
-            // ctx.strokeRect(x - ww/2, y - hh/2, ww, hh)
-
-            // ctx.closePath()
-
-            // ctx.beginPath();
-            // ctx.rect(posX, posY, w, h);
-            // ctx.fillStyle= "orange"
-            // ctx.fill();
-            // ctx.closePath()
-            })
-
-        })
+    // ctx.beginPath();
+    // ctx.rect(posX, posY, w, h);
+    // ctx.fillStyle= "orange"
+    // ctx.fill();
+    // ctx.closePath()
+  });
+});
 
 
-     // ctx.rect(50, 50, 100, 100);
-        // ctx.fill();
 
-        // ctx.rect(200, 50, 100, 100);
-        // ctx.fill();
+// ctx.rect(50, 50, 100, 100);
+// ctx.fill();
 
-        // ctx.rect(50, 200, 250, 100);
-        // ctx.fill();
+// ctx.rect(200, 50, 100, 100);
+// ctx.fill();
 
-
-   
+// ctx.rect(50, 200, 250, 100);
+// ctx.fill();
